0f83c894cc1f3938eac7c1b8ba3c7013ccd1928c
koztimesin <a.isumairu01@gmail.com>
2022-07-05 13:31:11 +0300

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Karthus Saffron on 7/5/22.
+//  Copyright © 2022 koztimesin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
